24d508a5f50f46e642734ef60130c1a8cf088d6f
profforum <75256519+profforum@users.noreply.github.com>
2022-04-19 14:49:43 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 var scene = document.getElementById('scene');
 var parallaxInstance = new Parallax(scene);
+
+ $(function () {
+var element = $("#hidden_block"), display;
+$(window).scroll(function () {
+display = $(this).scrollTop() <= 1;
+display != element.css('opacity') && element.stop().animate({ 'opacity': display }, 40);
+});
+});
+
+$(document).ready(function(){
+    $(this).scrollTop(0);
+});
